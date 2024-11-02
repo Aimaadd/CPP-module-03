64ee1bd8a287a7a3fcc982c96c5ecc14bf9ed0e3
Aimaadd <abentaye@fu-r3-p1.s19.be>
2024-11-02 17:24:35 +0100

    upgraded version with no mistakes better for next exercises

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,25 +1,27 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() {
-
+ClapTrap::ClapTrap() : _name(""), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+    std::cout << " random hp: " << _hitPoints << " ep: " << _energyPoints << " ad: " << _attackDamage << " " << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name ) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
-    std::cout << "Here comes the ClapTrap" << std::endl;
-    std::cout << _name + " | " << "hp: " << _hitPoints << " ep: " << _energyPoints << " ad: " << _attackDamage << " " << std::endl;
+    std::cout << "Here comes " << name << " ClapTrap" << std::endl;
+    std::cout << _name << " hp: " << _hitPoints << " ep: " << _energyPoints << " ad: " << _attackDamage << " " << std::endl;
 }
 
 ClapTrap::~ClapTrap() {
-
+    std::cout << "ClapTrap is gone" << std::endl;
 }
 
 void ClapTrap::attack(std::string const &target) {
-    if(this->_hitPoints <= 0 || this->_energyPoints <= 0) {
-        std::cout << "Can't attack" << std::endl;
-        return;
-    }
-    else {
-        std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << _attackDamage << " points of damage" << std::endl;
+    if (_energyPoints <= 0) {
+        std::cout << "No energy left to attack" << std::endl;
+    } else {
+        if (_name.empty()) {
+            std::cout << "random attacks " << target << ", causing " << _attackDamage << " points of damage" << std::endl;
+        } else {
+            std::cout << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage" << std::endl;
+        }
         _energyPoints--;
     }
 }
@@ -30,14 +32,21 @@ void ClapTrap::takeDamage(unsigned int amount) {
         return;
     }
     if (amount >= _hitPoints) {
-        std::cout << _name << " died from this attack" << std::endl;
-    }
-    else {
-        std::cout << _name << " received an attack that caused " << amount << " damage !" << std::endl;
-        _hitPoints = _hitPoints - amount;
+        if (_name.empty()) {
+            std::cout << "random died from this attack" << std::endl;
+        } else {
+            std::cout << _name << " died from this attack" << std::endl;
+        }
+    } else {
+        if (_name.empty()) {
+            std::cout << "random received an attack that caused " << amount << " damage !" << std::endl;
+            std::cout << "random now has " << _hitPoints - amount << "hp" << std::endl;
+        } else {
+            std::cout << _name << " received an attack that caused " << amount << " damage !" << std::endl;
+            std::cout << _name << " now has " << _hitPoints - amount << "hp" << std::endl;
+        }
+        _hitPoints -= amount;
         _energyPoints--;
-        std::cout << _name << " now has " << _hitPoints << "hp" << std::endl;
-        return;
     }
 }
 
@@ -45,11 +54,13 @@ void ClapTrap::beRepaired(unsigned int amount) {
     if (amount <= 0) {
         std::cout << "Can't heal" << std::endl;
         return;
-    }
-    else {
+    } else {
         _energyPoints--;
-        _hitPoints = _hitPoints + amount;
-        std::cout << _name << " has now " << _hitPoints << " hp !" << std::endl;
-        return;
+        _hitPoints += amount;
+        if (_name.empty()) {
+            std::cout << "random has now " << _hitPoints << " hp !" << std::endl;
+        } else {
+            std::cout << _name << " has now " << _hitPoints << " hp !" << std::endl;
+        }
     }
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,9 +4,16 @@ int main() {
     ClapTrap a("Cool");
     ClapTrap b;
         // check negative values entered
+    std::cout << "=======ClapTrap a======" << std::endl;
     a.attack("Random");
     a.takeDamage(1); 
     a.takeDamage(1); 
     a.takeDamage(1);
     a.beRepaired(3);
+    std::cout << "=======ClapTrap b======" << std::endl;
+    b.attack("Random");
+    b.takeDamage(1); 
+    b.takeDamage(1); 
+    b.takeDamage(1);
+    b.beRepaired(3);
 }
